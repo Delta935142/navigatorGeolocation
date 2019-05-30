bfc6ca9edfc08b755a4761d709f56ec38f308850
Mark <mark@weya.com.tw>
2019-05-30 13:44:36 +0800

    NavigatorGeolocation

--- navigator-geolocation.js
+++ navigator-geolocation.js
@@ -4,22 +4,21 @@ var lon;
 $("#locale").on('click', function (e){
     e.preventDefault();
     if (navigator.geolocation) {
-        var getPosition = function (opt) {
-            return new Promise(function (res, rej) {
-                navigator.geolocation.getCurrentPosition(res, rej, opt);
-            });
-        }
-    
-        getPosition().then((position) => {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            console.log(lat, lon);
-            //to do...
-        }).catch((err) => {
-            console.error(err.message);
-        });
-    
+        navigator.geolocation.getCurrentPosition(showPosition, errCallback);
     } else { 
         alert("您的瀏覽器不支援 顯示地理位置 API");
     }
 });
+
+//position
+function showPosition(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    console.log(lat, lon);
+    //to do...
+}
+
+//error callback
+function errCallback(err) {
+    console.log(err);
+}
